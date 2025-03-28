bd401e3916e5e615781641d6b393feeeabbb1445
IsmaCortGtz <ismacortgtz@gmail.com>
2025-03-27 20:11:39 -0600

    Update mongodb uri connection

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,9 +2,8 @@ import mongoose from 'mongoose';
 import print from '#util/print/index.js';
 
 try {
-  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_HOST, MONGODB_DATABASE } = process.env;
   print.log(`Connecting to MongoDB...`);
-  await mongoose.connect(`mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_HOST}/${MONGODB_DATABASE}`); 
+  await mongoose.connect(process.env.MONGODB_URI); 
 } catch(err) {
   print.error(err.message);
 }
